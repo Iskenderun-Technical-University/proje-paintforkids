ad8bdf777ccf797aa67714639de65a9402b38a13
Okan Cingöz <100669925+okancngz@users.noreply.github.com>
2022-05-27 22:30:35 +0300

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,7 +39,7 @@ namespace PaintApp
             this.btn_fill = new System.Windows.Forms.Button();
             this.btn_eraser = new System.Windows.Forms.Button();
             this.btn_Color = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.pic_color = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pic = new System.Windows.Forms.PictureBox();
@@ -61,7 +61,7 @@ namespace PaintApp
             this.panel1.Controls.Add(this.btn_fill);
             this.panel1.Controls.Add(this.btn_eraser);
             this.panel1.Controls.Add(this.btn_Color);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pic_color);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -79,6 +79,7 @@ namespace PaintApp
             this.btn_save.TabIndex = 10;
             this.btn_save.Text = "Kaydet";
             this.btn_save.UseVisualStyleBackColor = false;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
             // 
             // btn_clear
             // 
@@ -90,9 +91,11 @@ namespace PaintApp
             this.btn_clear.TabIndex = 9;
             this.btn_clear.Text = "Sil";
             this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
             // 
             // color_picker
             // 
+            this.color_picker.Cursor = System.Windows.Forms.Cursors.Hand;
             this.color_picker.Image = global::PaintApp.Properties.Resources.color_palette;
             this.color_picker.Location = new System.Drawing.Point(0, -1);
             this.color_picker.Name = "color_picker";
@@ -100,10 +103,12 @@ namespace PaintApp
             this.color_picker.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.color_picker.TabIndex = 8;
             this.color_picker.TabStop = false;
+            this.color_picker.MouseClick += new System.Windows.Forms.MouseEventHandler(this.color_picker_MouseClick);
             // 
             // btn_circle
             // 
             this.btn_circle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_circle.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_circle.Image = global::PaintApp.Properties.Resources.circle;
             this.btn_circle.Location = new System.Drawing.Point(1324, 38);
             this.btn_circle.Name = "btn_circle";
@@ -117,6 +122,7 @@ namespace PaintApp
             // btn_rectangle
             // 
             this.btn_rectangle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_rectangle.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_rectangle.ForeColor = System.Drawing.SystemColors.ControlText;
             this.btn_rectangle.Image = global::PaintApp.Properties.Resources.rectangle;
             this.btn_rectangle.Location = new System.Drawing.Point(1207, 38);
@@ -131,6 +137,7 @@ namespace PaintApp
             // btn_pencil
             // 
             this.btn_pencil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_pencil.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_pencil.Image = global::PaintApp.Properties.Resources.pencil;
             this.btn_pencil.Location = new System.Drawing.Point(973, 38);
             this.btn_pencil.Name = "btn_pencil";
@@ -144,6 +151,7 @@ namespace PaintApp
             // btn_line
             // 
             this.btn_line.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_line.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_line.Image = global::PaintApp.Properties.Resources.line;
             this.btn_line.Location = new System.Drawing.Point(1090, 38);
             this.btn_line.Name = "btn_line";
@@ -157,6 +165,7 @@ namespace PaintApp
             // btn_fill
             // 
             this.btn_fill.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_fill.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_fill.Image = global::PaintApp.Properties.Resources.bucket;
             this.btn_fill.Location = new System.Drawing.Point(856, 38);
             this.btn_fill.Name = "btn_fill";
@@ -165,10 +174,12 @@ namespace PaintApp
             this.btn_fill.Text = "Doldur";
             this.btn_fill.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btn_fill.UseVisualStyleBackColor = false;
+            this.btn_fill.Click += new System.EventHandler(this.btn_fill_Click);
             // 
             // btn_eraser
             // 
             this.btn_eraser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_eraser.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_eraser.Image = global::PaintApp.Properties.Resources.eraser;
             this.btn_eraser.Location = new System.Drawing.Point(739, 38);
             this.btn_eraser.Name = "btn_eraser";
@@ -182,6 +193,7 @@ namespace PaintApp
             // btn_Color
             // 
             this.btn_Color.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_Color.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Color.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
             this.btn_Color.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
             this.btn_Color.Image = global::PaintApp.Properties.Resources.color;
@@ -192,17 +204,18 @@ namespace PaintApp
             this.btn_Color.Text = "Renkler";
             this.btn_Color.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btn_Color.UseVisualStyleBackColor = false;
+            this.btn_Color.Click += new System.EventHandler(this.btn_Color_Click);
             // 
-            // button1
+            // pic_color
             // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button1.Location = new System.Drawing.Point(507, 62);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(61, 48);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = false;
+            this.pic_color.BackColor = System.Drawing.Color.White;
+            this.pic_color.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.pic_color.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pic_color.Location = new System.Drawing.Point(507, 62);
+            this.pic_color.Name = "pic_color";
+            this.pic_color.Size = new System.Drawing.Size(61, 48);
+            this.pic_color.TabIndex = 0;
+            this.pic_color.UseVisualStyleBackColor = false;
             // 
             // panel3
             // 
@@ -230,6 +243,7 @@ namespace PaintApp
             this.pic.TabIndex = 2;
             this.pic.TabStop = false;
             this.pic.Paint += new System.Windows.Forms.PaintEventHandler(this.pic_Paint);
+            this.pic.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pic_MouseClick);
             this.pic.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_MouseDown);
             this.pic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_MouseMove);
             this.pic.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pic_MouseUp);
@@ -259,7 +273,7 @@ namespace PaintApp
         private Button btn_fill;
         private Button btn_eraser;
         private Button btn_Color;
-        private Button button1;
+        private Button pic_color;
         private Panel panel2;
         private Button btn_circle;
         private Button btn_rectangle;
